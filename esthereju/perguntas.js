69a544b26db4884c9ea7b1dd22cd041950684a9c
estherhasya <161460289+estherhasya@users.noreply.github.com>
2024-09-17 08:53:45 -0300

    Add files via upload

---
+++ esthereju/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Jogos',
+    'Qual dos personagens principais de The Last of us?',
+    'Ellie e Joel'
+)
+
+criaCartao(
+    'Filmes',
+    'Em como treinar seu dragão, qual o nome do pai do soluço?',
+    'Stoico'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quantos litros de sangue tem uma pessoa em média?',
+    'De 4 a 6 litros de sangue'
+)
+
+criaCartao(
+    'Livros',
+    'Em percy jackson, qual o nome do puddle que o trio encontra no primeiro livro? ',
+    'Gladiola'
+)
